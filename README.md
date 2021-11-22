7f140f3a3adef103e844b15006dbcebf433d90e9
bearddan2000 <bearddan2000@yahoo.com>
2021-11-22 18:58:23 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# java-cli-gradle-spring-testng-car-data
+
+## Description
+A POC for spring app. Testing done with TestNG.
+
+## Tech stack
+- java
+- gradle
+  - spring
+  - testng
+
+## Docker stack
+- gradle:jdk11
+
+## To run
+`sudo ./install.sh -u`
+
+## To stop
+`sudo ./install.sh -d`
+
+## For help
+`sudo ./install.sh -h`
---
+++ bin/src/main/java/example/Main.java
@@ -0,0 +1,19 @@
+package example;
+
+public class Main {
+  /**
+  * A greeting
+  *
+  * @param val greeting to be return
+  * @return      the greeting String
+  * @see         String
+  */
+  public static String greeting(String val){
+    return val;
+  }
+
+  public static void main(String[] args){
+      System.out.println(greeting("Hello World"));
+  }
+
+}
